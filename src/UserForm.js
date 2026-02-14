bae1d697e5334dc4f39e65aaaf03f6e595cba0fd
Hasindu-Hettiarachchi <heghettiarachchi@students.nsbm.ac.lk>
2026-02-14 13:57:32 +0530

    UserFrom and User

---
+++ src/UserForm.js
@@ -0,0 +1,93 @@
+import {Grid, Typography } from "@mui/material";
+
+const UserForm = props => {
+    return (
+        <Grid
+            container
+            spacing={2}
+            sx={{
+                backgroundColor: '#f5ffce',
+                marginBottom: '30px',
+                display: 'block',
+            }}
+
+        >
+            <Grid item xs={12}>
+                <Typography component={'h1'} sx={{ color: '#000000' }}>User Form</Typography>
+            </Grid>
+
+            <Grid item xs={12} sm={6} sx={{display: 'flex'}}>
+                <Typography
+
+                    component={'label'}
+                    htmlFor="id"
+                    sx={{
+                        color: '#000000',
+                        marginRight: '20px',
+                        fontSize: '16px',
+                        width: '100px',
+                        display: 'block'
+                    }}
+                >
+                    ID
+                </Typography>
+
+                <input
+                    type="number"
+                    id="id"
+                    name="id"
+                    sx={{width: '480px'}}
+                    value={''}
+                    onChange={e => {}} 
+                />
+                
+            </Grid>
+            <Grid item xs={12} sm={6} sx={{display: 'flex'}}>
+                <Typography
+
+                    component={'label'}
+                    htmlFor="name"
+                    sx={{
+                        color: '#000000',
+                        marginRight: '20px',
+                        fontSize: '16px',
+                        width: '100px',
+                        display: 'block'
+                    }}
+                >
+                    Name
+                </Typography>
+
+                <input
+                    type="text"
+                    id="name"
+                    name="name"
+                    sx={{width: '480px'}}
+                    value={''}
+                    onChange={e => {}} 
+                />
+                
+            </Grid>
+
+            <button
+                sx={{
+                    margin: 'auto',
+                    marginBottom: '20px',
+                    backgroundColor: '#00c6e6',
+                    color: '#000000',
+                    marginLeft: '15px',
+                    marginTop: '20px',
+                    '&:hover': {
+                        opacity: '0.7',
+                        backgroundColor: '#00c6e6',
+                    }
+                }}
+            >
+                Add
+            </button>
+            
+        </Grid >
+    );
+}
+
+export default UserForm;
